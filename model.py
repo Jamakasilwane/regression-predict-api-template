0f595fe308f22a0d6fce6b659098b01525457070
sandile-bit <mkizesandile@yahoo.com>
2020-06-01 22:59:39 +0200

    Modified the preprocessing code

--- model.py
+++ model.py
@@ -67,7 +67,7 @@ def _preprocess_data(data):
     
     ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
     predict_vector = ct.fit_transform(X_test)
-    #predict_vector = np.delete(predict_vector,1,axis=1)
+    
     # ------------------------------------------------------------------------
 
     return predict_vector
